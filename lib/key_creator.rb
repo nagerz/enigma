9d365a9ae8860a38a97b819f9187814b2685da44
nagerz <znager@gmail.com>
2019-01-05 22:41:37 -0700

    Added key_creator module to create key shifts from given key

---
+++ lib/key_creator.rb
@@ -0,0 +1,14 @@
+module KeyCreator
+
+  def create_keys(key)
+    pairs = []
+    key.chars.each_cons(2) {|a| pairs << a}
+    pairs.map do |pair|
+      pair.join.to_i
+    end
+  end
+
+  def generate_key
+  end
+
+end
