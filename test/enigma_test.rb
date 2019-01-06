64b1d7b867fe5c527a130fdac072a538db94e793
nagerz <znager@gmail.com>
2019-01-05 22:36:55 -0700

    Setup enigma tests to test for future encrpyt/decrypt methods

---
+++ test/enigma_test.rb
@@ -0,0 +1,86 @@
+require './test/test_helper'
+require 'date'
+require './lib/enigma'
+
+
+class EnigmaTest < Minitest::Test
+
+  def setup
+    @enigma = Enigma.new
+  end
+
+  def test_it_exists
+    assert_instance_of Enigma, @enigma
+  end
+
+  def test_it_has_attributes
+    assert_equal [], @enigma.keys
+    assert_equal [], @enigma.offsets
+  end
+
+  def test_it_encrypts
+    skip
+    expected = {
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
+      }
+
+    actual = @enigma.encrypt("hello world", "02715", "040895")
+
+    assert_equal expected, actual
+  end
+
+  def test_it_decrypts
+    skip
+    expected = {
+      decryption: "hello world",
+      key: "02715",
+      date: "040895"
+      }
+
+    actual = @enigma.decrypt("keder ohulw", "02715", "040895")
+
+    assert_equal expected, actual
+  end
+
+  def test_it_encrypts_with_todays_date
+    skip
+    expected = {
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
+      }
+
+    actual = @enigma.encrypt("hello world", "02715")
+
+    assert_equal expected, actual
+  end
+
+  def test_it_decrypts_with_todays_date
+    skip
+    expected = {
+      decryption: "hello world",
+      key: "02715",
+      date: "040895"
+      }
+
+    actual = @enigma.decrypt(encrypted[:encryption], "02715")
+
+    assert_equal expected, actual
+  end
+
+  def test_it_encrypts_with_random_key_and_todays_date
+    skip
+    expected = {
+      decryption: "hello world",
+      key: "02715",
+      date: "040895"
+      }
+
+    actual = @enigma.encrypt("hello world")
+
+    assert_equal expected, actual
+  end
+
+end
