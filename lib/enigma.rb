d5375e032e83adfa74faec2b834fd0774db3b1c5
nagerz <znager@gmail.com>
2019-01-05 23:38:03 -0700

    Added to encrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -3,16 +3,25 @@ require './lib/key_creator'
 class Enigma
   include KeyCreator
 
-  attr_reader :keys, :offsets
+  attr_reader :keys, :offsets, :shifts, :char_set
 
   def initialize
     @keys = []
     @offsets = []
+    @shifts = []
+    @char_set = ("a".."z").to_a << " "
   end
 
   def encrypt(message, key, date)
     create_keys(key)
     create_offsets(date)
+    @shifts = [@keys,@offsets].transpose.map {|pair| pair.sum}
+
+    split_message = message.split(//)
+    split_message.each do |letter|
+      if @char_set.include?(letter)
+        if split_message.index(letter) % 4 == 0
+          letter.to_i
   end
 
   def create_offsets(date)
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -14,9 +14,12 @@ class EnigmaTest < Minitest::Test
   end
 
   def test_it_has_attributes
+    expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+
     assert_equal [], @enigma.keys
     assert_equal [], @enigma.offsets
     assert_equal [], @enigma.shifts
+    assert_equal expected, @enigma.char_set
   end
 
   def test_it_creates_shift_keys
